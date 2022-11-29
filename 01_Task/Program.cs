d2ff0a5e15eea416f0f5e8224d082f1e7d141f65
ANGunchenko <gunchenko_a_n@mail.ru>
2022-11-29 23:15:29 +0300

    01_Task

---
+++ 01_Task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+int square = number * number;
+
+Console.Write ("Произведение числа на само себя = ");
+Console.WriteLine (square);
+
+
+// Разбор кода
---
+++ 05 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите трёхзначное число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+string str = Convert.ToString (num);
+Console.Write (str [2]);
+
+
+
+// Console.Write ("Введите трёхзначное число: ");
+// int num = Convert.ToInt32 (Console.ReadLine());
+// string str = Convert.ToString (num);
+// Console.Write (str [2]);
+
+
+
+//while ()
+// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
