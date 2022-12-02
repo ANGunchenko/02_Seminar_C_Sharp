f86cf158b4f59abf809a20e29ec3934419f87f13
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-02 21:58:31 +0300

    02_08_Task

---
+++ 02_08_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+
+// if (number1 % 7 == 0 &&  number1 % 23 == 0)
+// {
+//     Console.WriteLine("Ваше число кратно 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine("Ваше число не кратно 7 и 23");
+// }
+
+
+string number3 = number1 % 7 == 0 && number1 % 23 == 0 ? "Ваше число кратно 7 и 23" : "Ваше число не кратно 7 и 23";
+Console.WriteLine(number3);
