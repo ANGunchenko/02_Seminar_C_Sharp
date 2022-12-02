c53c06fe331a84700d61d328ef6c6516d088a4fc
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-02 21:58:54 +0300

    02_09_Task

---
+++ 02_09_Task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string number3 = (Math.Pow(number1, 2) == number2) || (Math.Pow(number2, 2) == number1) ? "Да" : "Нет";
+Console.WriteLine(number3);
+
+// if ((Math.Pow(number1, 2) == number2) || (Math.Pow(number2, 2) == number1))
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
