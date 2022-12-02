d168d2f8c9531107ca2fe298c86c855635dc71bf
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-02 21:58:09 +0300

    02_07_Task

---
+++ 02_07_Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 % number1 == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.WriteLine("Второе число не кратно первому " + number2 % number1);
+}
