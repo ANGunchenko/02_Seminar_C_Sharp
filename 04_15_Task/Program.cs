4daf980db384cd5a25dfcae48356808abeaeeac8
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-09 22:15:56 +0300

    04_15_Task

---
+++ 04_15_Task/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+
+System.Console.WriteLine("Введите размер массива");
+int arrLenght = inputNumber();
+int[] arr = Auto(arrLenght);
+
+// Random rand = new Random();
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = rand.Next(0, 2);
+// }
+System.Console.WriteLine(string.Join(" | ", arr));
+
+int[] Auto(int len)
+{
+    int[] arr = new int[len];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 2);
+    }
+    return arr;
+}
+
+int inputNumber()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
