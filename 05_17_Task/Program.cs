8009d928537b8149431d73a0af889666a1a539b3
ANGunchenko <gunchenko_a_n@mail.ru>
2022-12-13 23:21:46 +0300

    05_seminar

---
+++ 05_17_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿// Решение в группах задач:
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+
+int[] arr = Generation();//{ -4, -8, 8, 2 };
+Metod(arr);
+
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+
+int[] Metod(int[] ar)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+
+    return arr;
+}
+
+int[] Generation()
+{
+    Random rand = new Random();
+    int[] array = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            array[i] = rand.Next(-99, 100);
+           
+            
+        }
+        System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+        return array;
+}
---
+++ 05_19_Task/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+// int[] array = Generation(20, 1, 200);
+// int result = Counter(array);
+// System.Console.WriteLine("Кол-во элементов:  " + result);
+
+
+
+
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100)
+        {
+            count++;
+        }
+    }
+    return count;
+
+
+}
+
+int[] Generation(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange);
+
+
+    }
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+    return array;
+}
+
+
+
